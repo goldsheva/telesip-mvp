85c397a5003fe7f3e112074e9b1f26cb0d7c507d
Vitaliy Shevtsov <@shevtsov>
2026-02-12 17:42:51 +0200

    refactor

--- app/lib/features/calls/state/call_reconnect_scheduler_api.dart
+++
@@ -1,7 +0,0 @@
-abstract class CallReconnectSchedulerApi {
-  void cancel();
-  Duration get currentDelay;
-  int get currentAttemptNumber;
-  int get backoffIndex;
-  void schedule({required String reason, required void Function() onFire});
-}
